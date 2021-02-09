1c4fea1ea80892bddccfbb13e6a13e7d31580a77
Nick Messenger <nickmessenger@Nicks-MacBook-Pro.local>
2021-02-09 14:57:08 -0500

    First Upload

---
+++ Data Cleaning Script.R
@@ -0,0 +1,8 @@
+#School Levy Analysis 
+#Based on Kotchen-Powers
+#AEDE 2021 - Nick Messenger
+install.packages("tidyverse")
+library(tidyverse)
+
+#Data Frame
+levy <- as.data.frame(read.csv("SchoolLevyRaw.csv"))
